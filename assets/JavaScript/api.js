ae0c61f6f8f764c8de1c1ad599a244bb454a8b16
Nacho Mola <nachomola02@gmail.com>
2023-06-13 08:15:28 -0300

    modified:   assets/JavaScript/api.js

--- assets/JavaScript/api.js
+++ assets/JavaScript/api.js
@@ -1,6 +1,9 @@
 const listEl = document.querySelector("ul");
 
-fetch("http://api.mediastack.com/v1/news?access_key=a8aeb2951489e97978cb1e3b81aa254e&countries=ar&keywords=cerveza&limit=24")
+fetch("http://api.mediastack.com/v1/news?access_key=a8aeb2951489e97978cb1e3b81aa254e&countries=ar&keywords=cerveza&limit=24"
+       , {        
+        referrerPolicy: "unsafe_url" 
+      })
         .then(res => res.json())
         .then(data => {
                 data.data.forEach(post => {
