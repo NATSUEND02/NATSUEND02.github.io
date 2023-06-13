acc10177d0b026a1b765a6b7bc14e028ec1b4ee7
Nacho Mola <nachomola02@gmail.com>
2023-06-13 08:54:33 -0300

    modified:   assets/JavaScript/api.js

--- assets/JavaScript/api.js
+++ assets/JavaScript/api.js
@@ -1,7 +1,6 @@
 const listEl = document.querySelector("ul");
 
-fetch("http://api.mediastack.com/v1/news?access_key=a8aeb2951489e97978cb1e3b81aa254e&countries=ar&keywords=cerveza&limit=24"
-       )
+fetch("http://api.mediastack.com/v1/news?access_key=a8aeb2951489e97978cb1e3b81aa254e&countries=ar&keywords=cerveza&limit=24")
         .then(res => res.json())
         .then(data => {
                 data.data.forEach(post => {
